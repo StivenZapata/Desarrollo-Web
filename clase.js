037dbd10dfa3d3c990cbd1405ee18f78e079b0db
StivenZapata <stivenzapataaguirre@gmail.com>
2023-08-01 11:38:32 -0500

    Actividad clase 1
    tareas comandos basicos  de js

---
+++ clase.js
@@ -0,0 +1,25 @@
+
+let player = 'Stiven Zapata'
+player = 2023;
+
+var playerDos = 'Salomon';
+/* Imprime el Valor de esta variable
+
+*/
+console.log(playerDos.length);             
+
+
+let a;
+a = 3.1416
+
+// Ejemplos de variables
+let name = "Stiven";
+const found = false;
+var age = 23;
+console.log(name, found, age);
+// impimir: STIVEN false 23
+
+//Declaracion de variable 
+var age;
+let weight;
+const numberOfFingers = 10;
